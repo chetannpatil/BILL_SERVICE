e15859110219f8a08d64a1ad0128d558b6787c8d
CHETAN <chetu.is@gmail.com>
2020-08-26 14:40:45 +0530

    first commit

---
+++ src/main/java/io/chetan/BillServiceApplication.java
@@ -0,0 +1,13 @@
+package io.chetan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillServiceApplication.class, args);
+	}
+
+}
