e4911625f8f556548b5de3e83a7f81de2e129b4a
CHETAN PATIL <chetu.is@gmail.com>
2020-11-10 19:59:56 +0530

    imlemetend ribbon,sleuth and zipkin

--- src/main/java/io/chetan/bill/BillServiceApplication.java
+++ src/main/java/io/chetan/bill/BillServiceApplication.java
@@ -1,12 +1,47 @@
 package io.chetan.bill;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+import brave.sampler.Sampler;
+
+//@EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaClient
 public class BillServiceApplication {
 
-	public static void main(String[] args) {
+	//private static final Logger LOGGER = Logger.getLogger(BillServiceApplication.class.getName());
+	
+	private static Logger LOGGER = LogManager.getLogger(BillServiceApplication.class);
+
+	@Bean
+	public Sampler defaultSampler() 
+	{
+		LOGGER.info("\n\n BillServiceApplication - defaultSampler- \n\n");
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate()
+	{
+		LOGGER.info("\n\n BillServiceApplication - getRestTemplate- \n\n");
+		
+		return new RestTemplate();
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		LOGGER.info("\n\n\n BillServiceApplication main \n\n\n");
 		SpringApplication.run(BillServiceApplication.class, args);
 	}
 
--- src/main/java/io/chetan/bill/controller/BillController.java
+++ src/main/java/io/chetan/bill/controller/BillController.java
@@ -1,15 +1,22 @@
 package io.chetan.bill.controller;
 
 import java.util.List;
-import java.util.logging.Logger;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.chetan.bill.model.Bill;
+import io.chetan.bill.model.Complaint;
 import io.chetan.bill.service.BillService;
 
 @RestController
@@ -20,7 +27,82 @@ public class BillController
 	@Autowired
 	private BillService billService;
 	
-	private static final Logger LOGGER = Logger.getLogger(BillController.class.getName());
+	//private static final Logger LOGGER = Logger.getLogger(BillController.class.getName());
+	
+	private static Logger LOGGER = LogManager.getLogger(BillController.class);
+	
+	private static final String OWNER_SERVICE_URI = "http://OwnerService/mypg/";
+	
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	@GetMapping(value = "/get2")
+	public String getString()
+	{
+		//System.out.println("\n BillController - get  with header = \n"+header);
+		
+		LOGGER.info("\n BillController - get  with  = \n");
+		
+		
+		
+		return "from BillController - get  with";
+	}
+	
+	
+	
+	
+	@GetMapping(value = "/readComplaint")
+	public Complaint get_temp_for_Logger()
+	{
+		System.out.println("\n BillController - getPg_temp_for_Logger(    = \n");
+		
+		LOGGER.info("\n BillController - getPg_temp_for_Logger(    = \n");
+		
+	
+		
+		
+		Complaint complaint = restTemplate.getForObject(OWNER_SERVICE_URI+"readComplaint", 
+				Complaint.class);
+				
+		
+		return complaint;
+	}
+	
+	
+	@GetMapping(value = "/getOwner")
+	public Iterable get_temp_for_ribbon()
+	{
+		System.out.println("\n BillController - get_temp_for_ribbon    = \n");
+		
+		LOGGER.info("\n BillController - get_temp_for_ribbon    = \n");
+		
+		
+		
+		ServiceInstance serviceInstance = loadBalancer.choose("OwnerService");
+		
+		LOGGER.info("\n BillController - get_temp_for_ribbon - serviceInstance  = \n"+serviceInstance);
+		
+		System.out.println(serviceInstance.getUri());
+		
+		LOGGER.info("\n BillController - get_temp_for_ribbon - serviceInstance.getUri  = \n"+serviceInstance.getUri());
+		
+		
+		Iterable iterable = restTemplate.getForObject(OWNER_SERVICE_URI+"readOwner", 
+				Iterable.class);
+				
+//		Bill bill = new Bill();
+//		bill.setAmount(3.231);
+//		bill.setRoomNumber(12);
+		
+		LOGGER.info("\n BillController - get_temp_for_ribbon - iterable.  = \n"+iterable);
+		
+		return iterable;
+	}
+	
 	
 	//loading all bills of an inmate
 	//                    findAll/{inMateId}
@@ -33,6 +115,33 @@ public class BillController
 //		
 //		return billService.getAllBillsOfInMate(inMateId);
 //	}
+	
+	//
+	@GetMapping(value = "/read")
+	public Bill get_temp_for_customFilter()
+	{
+		System.out.println("\n BillController - get_temp_for_customFilter    = \n");
+		
+		LOGGER.info("\n BillController - get_temp_for_customFilter    = \n");
+		
+		Bill bill = new Bill();
+		bill.setAmount(3.231);
+		bill.setRoomNumber(12);
+		return bill;
+	}
+	
+	@GetMapping(value = "/get")
+	public Bill get_temp(@RequestHeader("first-request") String header)
+	{
+		System.out.println("\n BillController - get  with header = \n"+header);
+		
+		LOGGER.info("\n BillController - get  with header = \n"+header);
+		
+		Bill bill = new Bill();
+		bill.setAmount(2312.231);
+		
+		return bill;
+	}
 	
 	
 	@GetMapping(value = "/findAll/{myInMate}")
